84a246aaa99924f51b3821a51dfdb5de9cb27f4a
chiyongs <tlscldyd@gmail.com>
2021-05-30 03:24:43 +0900

    국외 코로나 새벽시간 전날 데이터를 쿼리해 보여주도록 수정

--- src/main/java/wiwy/covid/controller/CoronaController.java
+++ src/main/java/wiwy/covid/controller/CoronaController.java
@@ -13,10 +13,7 @@ import wiwy.covid.apicall.coronadto.CoronaDto;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Controller
@@ -30,12 +27,24 @@ public class CoronaController {
         List<CoronaDto> hapGae = coronaRepository.findHapGae();
         CoronaDto coronaToday = hapGae.get(0);
 
+        List<AbrCoronaDto> recent = abrCoronaRepository.findRecent();
+
+
         LocalDateTime date = LocalDateTime.now();
         DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy년 MM월 d일");
         String curDate = date.format(df);
         curDate = curDate + " 00시";
 
-        List<AbrCoronaDto> abroads = abrCoronaRepository.findByDate(curDate);
+        List<AbrCoronaDto> abroads = new ArrayList<>();
+
+        // 업데이트 된 경우
+        if(curDate == recent.get(0).getStdDay()) {
+            abroads = abrCoronaRepository.findByDate(curDate);
+        } else { // 새벽이어서 업데이트 안 된 경우
+            date = date.minusDays(1);
+            curDate = date.format(df) + " 00시";
+            abroads = abrCoronaRepository.findByDate(curDate);
+        }
 
 //        // seq를 기준으로 오름차순 정렬
 //        AbrCoronaComparator comp = new AbrCoronaComparator();
